aea4398c634eaab13e62a01a871decd9ec4d19b6
cuiweigang <cuiweigang@139.com>
2015-11-30 22:09:20 +0800

    fix bug

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,37 +4,37 @@
 'use strict';
 import React from 'react';
 import '../Assets/css/App.css';
-//import ReactDOM from 'react-dom';
-//import Message from './Message';
-//import Message1 from './Message1';
-//import { Router, Route, Link } from 'react-router'
-//import TodoComponent from './RefluxDemo';
-//import RefluxDemo2 from './RefluxDemo2';
-//import RefluxDemo3 from './RefluxDemo3';
-//
-//import Menu from './Menu';
+import ReactDOM from 'react-dom';
+import Message from './Message';
+import Message1 from './Message1';
+import { Router, Route, Link } from 'react-router'
+import TodoComponent from './RefluxDemo';
+import RefluxDemo2 from './RefluxDemo2';
+import RefluxDemo3 from './RefluxDemo3';
 
-//var App = React.createClass({
-//  render () {
-//    return (
-//        <div>
-//          <Menu/>
-//          {this.props.children}
-//        </div>
-//    )
-//  }
-//});
-//
-//var router = (
-//    <Router>
-//      <Route path="/" component={App}>
-//        <Route path="m/:id" component={Message}/>
-//        <Route path="m1" component={Message1}/>
-//        <Route path="m2" component={TodoComponent}/>
-//        <Route path="m3" component={RefluxDemo2}/>
-//        <Route path="m4" component={RefluxDemo3}/>
-//      </Route>
-//    </Router>
-//);
+import Menu from './Menu';
 
-//ReactDOM.render(router, document.getElementById('container'));
+var App = React.createClass({
+  render () {
+    return (
+        <div>
+          <Menu/>
+          {this.props.children}
+        </div>
+    )
+  }
+});
+
+var router = (
+    <Router>
+      <Route path="/" component={App}>
+        <Route path="m/:id" component={Message}/>
+        <Route path="m1" component={Message1}/>
+        <Route path="m2" component={TodoComponent}/>
+        <Route path="m3" component={RefluxDemo2}/>
+        <Route path="m4" component={RefluxDemo3}/>
+      </Route>
+    </Router>
+);
+
+ReactDOM.render(router, document.getElementById('container'));
